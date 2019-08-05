79b50315c16c826b1bde043e397a29a5c9d3e80e
Anthony <anthonymarques@outlook.com>
2019-08-05 20:18:31 +0200

    rolled back ng-cirlce-progress

--- restSet/src/app/home/home.page.ts
+++ restSet/src/app/home/home.page.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  percent: number = 50;
+  radius: number = 100;
 
-  constructor() {}
-
+  fullTime: any = "00:01:30";
 }
